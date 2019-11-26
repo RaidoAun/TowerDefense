a1fc4e456fb55ba9a569345534c349d541d0af7e
Meleon420 <leokuris720@gmail.com>
2019-11-26 21:21:55 +0200

    kordinaadid tööle

--- test/main.py
+++ test/main.py
@@ -18,21 +18,26 @@ def printmaatrix(maatriks):
         for j1 in i1:
             rida += str(j1)
         print(rida)
-
-maatrix = makeMatrix(10, 10, [0, 0], [9, 9])
+start = [0, 0]
+end = [9, 9]
+maatrix = makeMatrix(10, 10, start, end)
 printmaatrix(maatrix)
-openList = [[0, 0, [0, 0], 0, 141, 141]] #[x, y, [parentx, parenty], G, H, F]
+openList = [[start[0], start[1], start, 0, 141, 141]] #[x, y, [parentx, parenty], G, H, F]
 openListF = [141]
 closedList = []
 while len(openList) > 0:
     current = openList.pop(openListF.index(min(openListF)))
     closedList.append(current)
-    for i in range(-1, 1):
-        for j in range(-1, 1):
-            uusx = current[0] + i
-            uusy = current[1] + j
+    for i in range(-1, 2, 2):
+        for j in range(2):
+            if j == 0:
+                uusx = current[0] + i
+                uusy = current[1]
+            else:
+                uusx = current[0]
+                uusy = current[1] + i
             print(uusx, uusy)
-            if (uusx) > 0 and (uusy) > 0: #kui ruut asub piirjoontes
+            if (uusx) >= 0 and (uusy) >= 0: #kui ruut asub piirjoontes
                 if maatrix[uusy][uusx] == 0: #kui ruut on avatud
                     h = int(math.sqrt(pow(uusx + 1, 2) + pow(uusy + 1, 2)) * 10)
                     g = current[3] + 10

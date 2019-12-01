002606d8f99b45d91187c090a27dd116991dedfc
UltraDragonDreamFeet <uglysanta19@gmail.com>
2019-12-01 23:51:28 +0200

    Monster class

---
+++ src/sample/Monster.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.scene.paint.Color;
+
+public class Monster {
+    int hp;
+    int dmg;
+    int id;
+    int x;
+    int y;
+    Color color;
+    Monster(int type, int x_coord, int y_coord){
+        x = x_coord;
+        y = y_coord;
+        id = type;
+        if (id==0){
+            hp = 10;
+            dmg = 5;
+            color = new Color(1,1,0,1);
+        }
+    }
+     public void drawMonster(){
+        Main.getGc().setFill(color);
+        int radius = 10;
+        Main.getGc().fillOval(x-radius, x-radius, radius*2, radius*2);
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+}
